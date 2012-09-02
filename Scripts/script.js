2dab704f543dca2c939e14a492c458f5e9aa5dd9
anton <anton.rifco@gmail.com>
2012-09-02 15:53:42 +0800

    update to fix search container
    update to fix search container

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,40 +3,25 @@
 */
 
 // Speed of the automatic slideshow
-var slideshowSpeed = 6000;
+var slideshowSpeed = 12000;
 
 // Variable to store the images we need to set as background
 // which also includes some text and url's.
 var photos = [ {
-		"title" : "Stairs",
 		"image" : "vacation.jpg",
-		"url" : "http://www.sxc.hu/photo/1271909",
-		"firstline" : "Going on",
-		"secondline" : "vacation?"
+		"url" : "http://www.sxc.hu/photo/1271909"
 	}, {
-		"title" : "Office Appartments",
 		"image" : "work.jpg",
-		"url" : "http://www.sxc.hu/photo/1265695",
-		"firstline" : "Or still busy at",
-		"secondline" : "work?"
+		"url" : "http://www.sxc.hu/photo/1265695"
 	}, {
-		"title" : "Mountainbiking",
 		"image" : "biking.jpg",
-		"url" : "http://www.sxc.hu/photo/1221065",
-		"firstline" : "Get out and be",
-		"secondline" : "active"
+		"url" : "http://www.sxc.hu/photo/1221065"
 	}, {
-		"title" : "Mountains Landscape",
 		"image" : "nature.jpg",
-		"url" : "http://www.sxc.hu/photo/1271915",
-		"firstline" : "Take a fresh breath of",
-		"secondline" : "nature"
+		"url" : "http://www.sxc.hu/photo/1271915"
 	}, {
-		"title" : "Italian pizza",
 		"image" : "food.jpg",
-		"url" : "http://www.sxc.hu/photo/1042413",
-		"firstline" : "Enjoy some delicious",
-		"secondline" : "food"
+		"url" : "http://www.sxc.hu/photo/1042413"
 	}
 ];
 
@@ -122,17 +107,7 @@ $(document).ready(function() {
 		});
 		
 		// Hide the header text
-		$("#headertxt").css({"display" : "none"});
-		
-		// Set the new header text
-		$("#firstline").html(photoObject.firstline);
-		$("#secondline")
-			.attr("href", photoObject.url)
-			.html(photoObject.secondline);
-		$("#pictureduri")
-			.attr("href", photoObject.url)
-			.html(photoObject.title);
-		
+		//$("#headertxt").css({"display" : "none"});
 		
 		// Fade out the current container
 		// and display the header text when animation is complete
