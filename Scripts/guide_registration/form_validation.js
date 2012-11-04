e29dc9b83dc625bbe2304abf026d163b04b7f310
budi santoso <budi.raharjo.santoso@gmail.com>
2012-11-04 14:40:13 +0800

    relocate js

---
+++ Scripts/guide_registration/form_validation.js
@@ -0,0 +1,146 @@
+/*
+* Author:      Marco Kuiper (http://www.marcofolio.net/)
+*/
+
+// Speed of the automatic slideshow
+var slideshowSpeed = 12000;
+
+// Variable to store the images we need to set as background
+// which also includes some text and url's.
+var photos = [ {
+		"image" : "vacation.jpg",
+		"url" : "http://www.sxc.hu/photo/1271909"
+	}, {
+		"image" : "work.jpg",
+		"url" : "http://www.sxc.hu/photo/1265695"
+	}, {
+		"image" : "biking.jpg",
+		"url" : "http://www.sxc.hu/photo/1221065"
+	}, {
+		"image" : "nature.jpg",
+		"url" : "http://www.sxc.hu/photo/1271915"
+	}, {
+		"image" : "food.jpg",
+		"url" : "http://www.sxc.hu/photo/1042413"
+	}
+];
+
+$(document).ready(function() {
+		
+	// Backwards navigation
+	$("#back").click(function() {
+		stopAnimation();
+		navigate("back");
+	});
+	
+	// Forward navigation
+	$("#next").click(function() {
+		stopAnimation();
+		navigate("next");
+	});
+	
+	var interval;
+	$("#control").toggle(function(){
+		stopAnimation();
+	}, function() {
+		// Change the background image to "pause"
+		$(this).css({ "background-image" : "url(../content/images/btn_pause.png)" });
+		
+		// Show the next image
+		navigate("next");
+		
+		// Start playing the animation
+		interval = setInterval(function() {
+			navigate("next");
+		}, slideshowSpeed);
+	});
+	
+	
+	var activeContainer = 1;	
+	var currentImg = 0;
+	var animating = false;
+	var navigate = function(direction) {
+		// Check if no animation is running. If it is, prevent the action
+		if(animating) {
+			return;
+		}
+		
+		// Check which current image we need to show
+		if(direction == "next") {
+			currentImg++;
+			if(currentImg == photos.length + 1) {
+				currentImg = 1;
+			}
+		} else {
+			currentImg--;
+			if(currentImg == 0) {
+				currentImg = photos.length;
+			}
+		}
+		
+		// Check which container we need to use
+		var currentContainer = activeContainer;
+		if(activeContainer == 1) {
+			activeContainer = 2;
+		} else {
+			activeContainer = 1;
+		}
+		
+		showImage(photos[currentImg - 1], currentContainer, activeContainer);
+		
+	};
+	
+	var currentZindex = -1;
+	var showImage = function(photoObject, currentContainer, activeContainer) {
+		animating = true;
+		
+		// Make sure the new container is always on the background
+		currentZindex--;
+		
+		// Set the background image of the new active container
+		$("#headerimg" + activeContainer).css({
+			"background-image" : "url(../content/images/" + photoObject.image + ")",
+			"display" : "block",
+			"z-index" : currentZindex
+		});
+		
+		// Hide the header text
+		//$("#headertxt").css({"display" : "none"});
+		
+		// Fade out the current container
+		// and display the header text when animation is complete
+		$("#headerimg" + currentContainer).fadeOut(function() {
+			setTimeout(function() {
+				$("#headertxt").css({"display" : "block"});
+				animating = false;
+			}, 500);
+		});
+	};
+	
+	var stopAnimation = function() {
+		// Change the background image to "play"
+		$("#control").css({ "background-image" : "url(../content/images/btn_play.png)" });
+		
+		// Clear the interval
+		clearInterval(interval);
+	};
+	
+	// We should statically set the first image
+	navigate("next");
+	
+	// Start playing the animation
+	interval = setInterval(function() {
+		navigate("next");
+	}, slideshowSpeed);
+	
+	
+	//pause on hover
+	//using data attributes
+	$(".live-tile").liveTile({pauseOnHover: true});
+	
+	//without data-attributes
+	//$("#tile1").liveTile({ delay:750, initDelay:750, pauseOnHover: true  });
+	//$("#tile2").liveTile({ delay:750, initDelay:750, mode:'flip', pauseOnHover: true });
+
+	
+});
